42a1c0cd1d5a4f7926c83d7ba3f3e7687e92bb7b
Herre3r4 <nahugiuben@gmail.com>
2022-11-09 20:14:46 -0300

    cambios varios1

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('HOLA MUNDO')
-})
+app.get("/", (req, res) => {
+  res.send("HOLA MUNDO");
+});
 
-app.listen(port, () => {
+app.listen(3000, () => console.log("http://localhost3000"));
+
+/* app.listen(port, () => {
   console.log(`Servidor corriendo ${port}`)
 })
+ */
