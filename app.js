3fcf2275a5a6abddef357f1340f20b6bd062869a
Herre3r4 <nahugiuben@gmail.com>
2022-11-09 22:28:54 -0300

    agregado de html

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const path = requiere("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("HOLA MUNDO");
+  res.sendFile(path.join(_dirname, "./views/index.html"));
 });
 
 app.listen(3000, () => console.log("http://localhost3000"));
