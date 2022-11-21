d1a68c160f33eba85beace16899f679174f5b37f
Herre3r4 <nahugiuben@gmail.com>
2022-11-21 19:05:20 -0300

    imagenes varias

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 //app.get("/", (req, res) => {
 // res.send("¡Hola mundo!");
 //});
 
-app.listen(3000, () =>
-  console.log("Servidor corriendo en http://localhost:3000")
-);
+app.use(express.static(path.resolve(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(_dirname, "./views/index.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "./views/reister.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "./views/login.html"));
+});
+
+app.listen(PORT, () =>
+  console.log("Servidor corriendo en http://localhost:3000")
+);
+
 /* app.listen(port, () => {
   console.log(`Servidor corriendo ${port}`)
 })
