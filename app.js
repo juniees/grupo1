13df9c471856a3c0d7b13a2c8701f3d5f8024e16
Herre3r4 <nahugiuben@gmail.com>
2022-11-22 23:37:56 -0300

    funciona app?

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-//app.get("/", (req, res) => {
-// res.send("¡Hola mundo!");
-//});
-
 app.use(express.static(path.resolve(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "./views/index.html"));
+  res.sendFile(path.resolve(_dirname, "views/index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "views/login.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "./views/reister.html"));
-});
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "./views/login.html"));
+  res.sendFile(path.resolve(_dirname, "views/reister.html"));
 });
 
 app.listen(PORT, () =>
-  console.log("Servidor corriendo en http://localhost:3000")
+console.log("Servidor corriendo en http://localhost:3000")
 );
+
+//app.get("/", (req, res) => {
+// res.send("¡Hola mundo!");
+//});
 
 /* app.listen(port, () => {
   console.log(`Servidor corriendo ${port}`)
