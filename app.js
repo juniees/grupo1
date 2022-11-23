572070509accfd4715ca26fe1b8d19fade67a48f
Herre3r4 <nahugiuben@gmail.com>
2022-11-23 00:17:14 -0300

    modificaciones

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "views/index.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "views/login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "views/reister.html"));
-});
-
-app.listen(PORT, () =>
-console.log("Servidor corriendo en http://localhost:3000")
+app.use(express.static(path.resolve(__dirname, 'public'))
 );
 
-//app.get("/", (req, res) => {
-// res.send("¡Hola mundo!");
-//});
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
 
-/* app.listen(port, () => {
-  console.log(`Servidor corriendo ${port}`)
-})
- */
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('listening on http://localhost:3000');
+});
